b4ae50341cd49ba89013cdfa24bbdc397d3d6a9f
Sergio Torres <sergio.lo.torres@gmail.com>
2021-07-11 19:09:50 -0500

    fixed react client to react hooks for apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/react-hooks';
 import { setContext } from '@apollo/client/link/context'
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
